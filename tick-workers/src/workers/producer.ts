6fb8fdfba1aa075de2b9b8a83b752c276523784f
brayn003 <brayn003@gmail.com>
2024-04-13 15:54:49 +0530

    fix: trying to restart on failure

--- tick-workers/src/workers/producer.ts
+++ tick-workers/src/workers/producer.ts
@@ -2,17 +2,17 @@ import { KiteTicker } from "kiteconnect";
 
 import { Broadcast } from "../services/broadcast";
 import { WatchedInstruments } from "../services/instruments";
-import { initialiseKiteSession } from "../services/kite-session";
+import { getKiteSession, initialiseKiteSession, loadKiteSession } from "../services/kite-session";
 import type { Tick } from "../types";
 
 class KiteTickerProducer {
   ticker?: KiteTicker;
   broadcast?: Broadcast;
   batchId: number = 0;
-  isReconnecting: boolean = false;
 
   async initialize() {
     const kiteSession = await initialiseKiteSession();
+    console.log("seems fine");
     if (!kiteSession || !kiteSession?.is_active) {
       throw new Error("No active session found");
     }
@@ -21,21 +21,24 @@ class KiteTickerProducer {
       api_key: kiteSession.data.api_key,
       access_token: kiteSession.data.access_token,
       // does not obey the max_retry and max_delay options
-      reconnect: true,
-      max_retry: 5,
-      max_delay: 5,
+      // reconnect: false,
+      // max_retry: 5,
+      // max_delay: 5,
     });
 
-    this.ticker.on("ticks", this.#onTicks.bind(this));
-    this.ticker.on("connect", this.#onConnect.bind(this));
-    this.ticker.on("disconnect", this.#onClose.bind(this));
+    this.ticker.autoReconnect(true, 3, 5);
+
+    this.ticker.on("ticks", this.#onTicks);
+    this.ticker.on("connect", this.#onConnect);
+    this.ticker.on("disconnect", this.#onDisconnect);
+    this.ticker.on("noreconnect", this.#onNoReconnect);
 
     const broadcast = new Broadcast();
     await broadcast.connectProducer();
     this.broadcast = broadcast;
   }
 
-  async #onTicks(ticks: Tick[]) {
+  readonly #onTicks = async (ticks: Tick[]) => {
     if (!this.broadcast) throw new Error("Broadcast not initialized");
     await this.broadcast.publish(ticks);
     this.batchId += 1;
@@ -44,9 +47,9 @@ class KiteTickerProducer {
       [WatchedInstruments[tick.instrument_token]]: tick.last_price,
     }));
     console.log(`[Published] BATCH: ${this.batchId} TOTAL: ${ticks.length} TICKS: ${JSON.stringify(ticksDef)}`);
-  }
+  };
 
-  async #onConnect() {
+  readonly #onConnect = async () => {
     if (!this.ticker) throw new Error("Ticker not initialized");
     const instruments = Object.keys(WatchedInstruments).map(
       (key) => WatchedInstruments[key as keyof typeof WatchedInstruments],
@@ -54,20 +57,21 @@ class KiteTickerProducer {
     this.ticker.subscribe(instruments);
     this.ticker.setMode(this.ticker.modeFull, instruments);
     console.log(`[Connected] SUBSCRIBED: ${JSON.stringify(instruments)}`);
-  }
+  };
 
-  async #onClose(ws: { code: number; reason: string }) {
+  readonly #onDisconnect = async (ws: { code: number; reason: string }) => {
     if (!this.ticker) throw new Error("Ticker not initialized");
-    this.ticker.disconnect();
-    if (!this.isReconnecting) {
-      this.isReconnecting = true;
-      await initialiseKiteSession();
-      this.isReconnecting = false;
-    }
-    console.log(`[Closed] CODE: ${ws?.code} REASON: ${ws?.reason}`);
-  }
+    console.log(`[Disconnected] CODE: ${ws?.code} REASON: ${ws?.reason}`);
+  };
 
-  start() {
+  readonly #onNoReconnect = async () => {
+    console.log(`[NoReconnect] KITE`);
+    await this.initialize();
+    console.log("thid is new");
+    this.start();
+  };
+
+  async start() {
     if (!this.ticker) throw new Error("Ticker not initialized");
     this.ticker.connect();
   }
@@ -77,4 +81,5 @@ export async function runProducer() {
   const producer = new KiteTickerProducer();
   await producer.initialize();
   producer.start();
+  console.log("damama");
 }
